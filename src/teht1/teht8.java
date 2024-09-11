93ba1de37439054c79af0735f95542c8a451a2a2
jopowastaken <jussi.oksaoja@gmail.com>
2024-09-11 12:38:47 +0300

    11/9/2024

--- src/teht1/teht8.java
+++ src/teht1/teht8.java
@@ -1,13 +1,27 @@
 package teht1;
 import Kaluja.*;
 
+import java.util.HashMap;
+
 public class teht8 {
     public static void main(String[] args) {
 
 
     }
     public static Boolean peli (String valinta){
-        String Tietokoneenvalinta;
-        return true;
-}
+        HashMap<Integer,String> kone = AlustaKone();
+        String koneenvalinta = kone.get(Rando.numero());
+        if (koneenvalinta.equals(valinta)){
+
+        }
+
+
+    }
+    public static HashMap<Integer,String> AlustaKone(){
+        HashMap <Integer, String> kone = new HashMap<Integer,String>();
+        kone.put(1,"Kivi");
+        kone.put(2,"Sakset");
+        kone.put(3,"Paperi");
+        return kone;
+    }
 }
